2bdc7982d194543966476136402babdd6fda5a3e
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-16 09:26:47 -0300

    feat: add DomainError and account specific errors

---
+++ src/domain/errors/account/account-errors.ts
@@ -0,0 +1,43 @@
+import { DomainError } from "../domain-error.js"
+
+export class InvalidEmailError extends DomainError {
+  constructor() {
+    super("Invalid email", "INVALID_EMAIL")
+  }
+}
+
+export class InvalidPasswordError extends DomainError {
+  constructor() {
+    super("Invalid password", "INVALID_PASSWORD")
+  }
+}
+
+export class InvalidStateError extends DomainError {
+  constructor() {
+    super("Invalid state", "INVALID_STATE")
+  }
+}
+
+export class InvalidCityError extends DomainError {
+  constructor() {
+    super("Invalid city", "INVALID_CITY")
+  }
+}
+
+export class InvalidNameError extends DomainError {
+  constructor() {
+    super("Invalid name", "INVALID_NAME")
+  }
+}
+
+export class InvalidPhoneNumberError extends DomainError {
+  constructor() {
+    super("Invalid phone number", "INVALID_PHONE_NUMBER")
+  }
+}
+
+export class InvalidUUIDError extends DomainError {
+  constructor() {
+    super("Invalid UUID", "INVALID_UUID")
+  }
+}
