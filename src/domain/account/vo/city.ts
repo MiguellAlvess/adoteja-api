e450086520d036efcf70bb29b50d969f6588486b
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-16 09:35:01 -0300

    refactor: throw InvalidCityError instead of generic error

---
+++ src/domain/account/vo/city.ts
@@ -0,0 +1,25 @@
+import { InvalidCityError } from "../../errors/account/account-errors.js"
+
+export class City {
+  private value: string
+
+  constructor(city: string) {
+    const v = city.trim()
+    if (!this.validateCity(v)) throw new InvalidCityError()
+    this.value = v!
+  }
+
+  validateCity(city?: string) {
+    if (!city) return false
+    if (city.length > 120) return false
+    if (!/^[\p{L}\s'.-]+$/u.test(city)) return false
+    if (!/^\p{L}.*\p{L}$/u.test(city)) return false
+    if (/\p{N}/u.test(city)) return false
+    if (/\s{2,}|--|''|\.\./.test(city)) return false
+    return true
+  }
+
+  getValue() {
+    return this.value
+  }
+}
