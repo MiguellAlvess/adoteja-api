2fbee1256097bc101b6d9a38fcdeb1485c7096da
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-16 09:59:26 -0300

    test: ensure Password throws InvalidPasswordError for invalid city name

--- test/account/unit/password.test.ts
+++ test/account/unit/password.test.ts
@@ -1,10 +1,11 @@
-import { Password } from "../../../src/domain/user/vo/password.js"
+import { Password } from "../../../src/domain/account/vo/password.js"
+import { InvalidPasswordError } from "../../../src/domain/errors/account/account-errors.js"
 
 describe("Password", () => {
   test.each(["", "short", "nouppercase1", "NOLOWERCASE1", "NoNumber"])(
     'should reject invalid "%s"',
     (password) => {
-      expect(() => new Password(password)).toThrow("Invalid password")
+      expect(() => new Password(password)).toThrow(InvalidPasswordError)
     }
   )
   test("should accept a valid password", () => {
