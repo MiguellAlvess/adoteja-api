66eb7712b950ce87fa6308343cb4139c33e1997f
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-16 09:35:31 -0300

    refactor: throw InvalidStateError instead of generic erro

---
+++ src/domain/account/vo/state.ts
@@ -0,0 +1,53 @@
+import { InvalidStateError } from "../../errors/account/account-errors.js"
+
+export class State {
+  private readonly value: string
+
+  constructor(state: string) {
+    const trimmed = state.trim()
+    if (!trimmed) throw new InvalidStateError()
+    const normalizedState = trimmed.toUpperCase()
+    if (!this.validateState(normalizedState)) {
+      throw new Error("Invalid state")
+    }
+    this.value = normalizedState
+  }
+
+  validateState(state: string): boolean {
+    if (!/^[A-Z]{2}$/.test(state)) return false
+    const UFS = new Set([
+      "AC",
+      "AL",
+      "AP",
+      "AM",
+      "BA",
+      "CE",
+      "DF",
+      "ES",
+      "GO",
+      "MA",
+      "MT",
+      "MS",
+      "MG",
+      "PA",
+      "PB",
+      "PR",
+      "PE",
+      "PI",
+      "RJ",
+      "RN",
+      "RS",
+      "RO",
+      "RR",
+      "SC",
+      "SP",
+      "SE",
+      "TO",
+    ])
+    return UFS.has(state)
+  }
+
+  getValue() {
+    return this.value
+  }
+}
