a1981d05e16ab271cb8d673ef18ade81da15ac97
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-20 18:05:08 -0300

    test: ensure Account entity update a valid account

--- test/account/unit/account.test.ts
+++ test/account/unit/account.test.ts
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt"
 describe("Account", () => {
   test("should create a valid account", async () => {
     const passwordHash = await bcrypt.hash("ValidPassword123", 4)
-
     const account = Account.create(
       "Miguel",
       "test@gmail.com",
@@ -13,7 +12,29 @@ describe("Account", () => {
       "City",
       "PB"
     )
+    expect(account.getName()).toBe("Miguel")
+    expect(account.getEmail()).toBe("test@gmail.com")
+  })
 
+  test("should update a valid account", async () => {
+    const passwordHash = await bcrypt.hash("ValidPassword123", 4)
+    const account = Account.create(
+      "Miguel",
+      "test@gmail.com",
+      passwordHash,
+      "(99) 99999-9999",
+      "City",
+      "PB"
+    )
+    const input = {
+      name: "Miguel",
+      email: "test@gmail.com",
+      phone: "(99) 99999-9999",
+      city: "City",
+      state: "PB",
+      password: passwordHash,
+    }
+    account.update(input)
     expect(account.getName()).toBe("Miguel")
     expect(account.getEmail()).toBe("test@gmail.com")
   })
