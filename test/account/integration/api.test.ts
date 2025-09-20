2032736b8956c3422f83a48cc5048465953399ac
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-20 17:25:37 -0300

    test: add integration tests (api) for account module

--- test/account/integration/api.test.ts
+++ test/account/integration/api.test.ts
@@ -3,6 +3,8 @@ import { buildApp } from "../../../src/app.js"
 import { startPostgresTestDb } from "../../../src/infra/database/test-db.js"
 import { AddressInfo } from "net"
 
+axios.defaults.validateStatus = () => true
+
 describe("API", () => {
   let baseURL: string
   let stopServer: () => Promise<void>
@@ -25,7 +27,7 @@ describe("API", () => {
     await stopServer()
   })
 
-  test("/POST /api/accounts should return 201 when account is created", async () => {
+  test("should return 201 when account is created", async () => {
     const input = {
       name: "Robert Martin",
       email: `test${Math.random()}@example.com`,
@@ -34,7 +36,110 @@ describe("API", () => {
       city: "City",
       state: "RJ",
     }
-    const res = await axios.post(`${baseURL}/api/accounts`, input)
-    expect(res.status).toBe(201)
+    const ouput = await axios.post(`${baseURL}/api/accounts`, input)
+    expect(ouput.status).toBe(201)
+  })
+
+  test("should return 200 when account is found", async () => {
+    const input = {
+      name: "Robert Martin",
+      email: `test${Math.random()}@example.com`,
+      password: "ValidPassword123",
+      phone: "(99) 99999-9999",
+      city: "City",
+      state: "RJ",
+    }
+    const ouputSignup = await axios.post(`${baseURL}/api/accounts`, input)
+    const outputGetAccount = await axios.get(
+      `${baseURL}/api/accounts/${ouputSignup.data.userId}`
+    )
+    expect(outputGetAccount.status).toBe(200)
+    expect(outputGetAccount.data.name).toBe(input.name)
+  })
+
+  test("should return 409 when email already exists", async () => {
+    const input = {
+      name: "Robert Martin",
+      email: `test${Math.random()}@example.com`,
+      password: "ValidPassword123",
+      phone: "(99) 99999-9999",
+      city: "City",
+      state: "RJ",
+    }
+    await axios.post(`${baseURL}/api/accounts`, input)
+    const output = await axios.post(`${baseURL}/api/accounts`, input)
+    expect(output.status).toBe(409)
+  })
+
+  test("should return 404 when account is not found", async () => {
+    const output = await axios.get(
+      `${baseURL}/api/accounts/0b735c5a-2f00-41b7-ad12-b97ac9a7cdba`
+    )
+    expect(output.status).toBe(404)
+  })
+
+  test("should return 400 when email is invalid", async () => {
+    const input = {
+      name: "Robert Martin",
+      email: "test",
+      password: "ValidPassword123",
+      phone: "(99) 99999-9999",
+      city: "City",
+      state: "RJ",
+    }
+    const output = await axios.post(`${baseURL}/api/accounts`, input)
+    expect(output.status).toBe(400)
+  })
+
+  test("should return 400 when password is invalid", async () => {
+    const input = {
+      name: "Robert Martin",
+      email: `test${Math.random()}@example.com`,
+      password: "123",
+      phone: "(99) 99999-9999",
+      city: "City",
+      state: "RJ",
+    }
+    const output = await axios.post(`${baseURL}/api/accounts`, input)
+    expect(output.status).toBe(400)
+  })
+
+  test("should return 400 when phone is invalid", async () => {
+    const input = {
+      name: "Robert Martin",
+      email: `test${Math.random()}@example.com`,
+      password: "ValidPassword123",
+      phone: "998832",
+      city: "City",
+      state: "RJ",
+    }
+    const output = await axios.post(`${baseURL}/api/accounts`, input)
+    expect(output.status).toBe(400)
+  })
+
+  test("should return 400 when state is invalid", async () => {
+    const input = {
+      name: "Robert Martin",
+      email: `test${Math.random()}@example.com`,
+      password: "ValidPassword123",
+      phone: "(99) 99999-9999",
+      city: "City",
+      state: "XX",
+    }
+    const output = await axios.post(`${baseURL}/api/accounts`, input)
+    expect(output.status).toBe(400)
+  })
+
+  test("should return 400 when city is invalid", async () => {
+    const input = {
+      name: "Robert Martin",
+      email: `test${Math.random()}@example.com`,
+      password: "ValidPassword123",
+      phone: "(99) 99999-9999",
+      city: "",
+      state: "RJ",
+    }
+    const output = await axios.post(`${baseURL}/api/accounts`, input)
+    expect(output.status).toBe(400)
   })
 })
