1a5a79a4b284774fd5a49a46056f36a719bd9996
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-01 20:09:29 -0300

    feat: add MulterPhotoStorageAdapter

---
+++ src/infra/storage/multer-photo-storage-adapter.ts
@@ -0,0 +1,18 @@
+import { promises as fs } from "fs"
+import path from "path"
+import {
+  PhotoInput,
+  PhotoStorage,
+} from "../../application/ports/storage/photo-storage.js"
+
+export class MulterPhotoStorageAdapter implements PhotoStorage {
+  constructor(private readonly baseDir = path.resolve("uploads")) {}
+
+  async upload(input: PhotoInput): Promise<string> {
+    await fs.mkdir(this.baseDir, { recursive: true })
+    const safeName = Date.now() + "-" + input.filename.replace(/\s+/g, "_")
+    const filePath = path.join(this.baseDir, safeName)
+    await fs.writeFile(filePath, input.buffer)
+    return `/uploads/${safeName}`
+  }
+}
