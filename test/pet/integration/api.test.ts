f9f544e22c1e40270ae66c545b005c56b6d4b805
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-02 17:05:33 -0300

    test: ensure GET/api/pets returns 200 when all pets are returned

--- test/pet/integration/api.test.ts
+++ test/pet/integration/api.test.ts
@@ -189,4 +189,32 @@ describe("Pet API", () => {
     const outputGetPet = await axios.get(`${baseURL}/api/pets/${invalidId}`)
     expect(outputGetPet.status).toBe(400)
   })
+
+  test("should return 200 when all pets are returned", async () => {
+    const ownerInput = {
+      name: "Owner",
+      email: `owner-${Math.random()}@example.com`,
+      password: "ValidPassword123",
+      phone: "(83) 99999-0000",
+      city: "Campina Grande",
+      state: "PB",
+    }
+    const outputOwner = await axios.post(`${baseURL}/api/accounts`, ownerInput)
+    expect(outputOwner.status).toBe(201)
+    const accessToken = outputOwner.data.accessToken as string
+    const petInput = {
+      name: "Spike",
+      species: "Dog",
+      gender: "MALE",
+      age: 3,
+      size: "SMALL",
+      description: "Adorable",
+    }
+    const outputPet = await axios.post(`${baseURL}/api/pets`, petInput, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    })
+    expect(outputPet.status).toBe(201)
+    const outputGetPets = await axios.get(`${baseURL}/api/pets`)
+    expect(outputGetPets.status).toBe(200)
+  })
 })
