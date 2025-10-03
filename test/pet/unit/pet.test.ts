5bd7555e84202680786a1dbcfdf91a2954736383
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-03 13:25:06 -0300

    test: ensure Pet entity update a valid pet

--- test/pet/unit/pet.test.ts
+++ test/pet/unit/pet.test.ts
@@ -18,4 +18,26 @@ describe("Pet", () => {
     expect(pet.getAge()).toBe(5)
     expect(pet.getPhotoUrl()).toBe("https://example.com/pet.jpg")
   })
+
+  test("should update a valid pet", () => {
+    const pet = Pet.create(
+      "ecb1772c-f2ac-46b0-bd6d-f4f505899714",
+      "Spike",
+      "Dog",
+      "MALE",
+      5,
+      "SMALL",
+      "Adorable and friendly dog",
+      "https://example.com/pet.jpg"
+    )
+    const input = {
+      name: "Spike",
+      age: 5,
+      photoUrl: "https://example.com/pet.jpg",
+    }
+    pet.update(input)
+    expect(pet.getName()).toBe("Spike")
+    expect(pet.getAge()).toBe(5)
+    expect(pet.getPhotoUrl()).toBe("https://example.com/pet.jpg")
+  })
 })
