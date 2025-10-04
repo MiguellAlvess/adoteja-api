a1620711cbc543a5e11982831845d6750d4a07a1
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-04 10:21:04 -0300

    feat: add approve adoption use case

---
+++ src/application/usecase/adoption/approve-adoption.ts
@@ -0,0 +1,32 @@
+import { AdoptionRepository } from "../../ports/repository/adoption-repository.js"
+import { AdoptionNotFoundError } from "../../errors/adoption/index.js"
+
+export class ApproveAdoption {
+  constructor(private readonly adoptionRepository: AdoptionRepository) {}
+
+  async execute(adoptionId: string): Promise<Output> {
+    const adoption = await this.adoptionRepository.findById(adoptionId)
+    if (!adoption) {
+      throw new AdoptionNotFoundError()
+    }
+    adoption.approve()
+    await this.adoptionRepository.update(adoption)
+    return {
+      id: adoption.getId(),
+      petId: adoption.getPetId(),
+      adopterId: adoption.getAdopterId(),
+      status: adoption.getStatusName(),
+      requestedAt: adoption.getRequestedAt(),
+      completedAt: adoption.getCompletedAt(),
+    }
+  }
+}
+
+type Output = {
+  id: string
+  petId: string
+  adopterId: string
+  status: "PENDING" | "APPROVED" | "REJECTED" | "COMPLETED"
+  requestedAt: Date
+  completedAt: Date | null
+}
